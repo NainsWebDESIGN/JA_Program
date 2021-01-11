eda24e2c4a87ab6dc89779445593c940a9e8df21
Nains <jason20226@gmail.com>
2021-01-11 14:15:16 +0800

    2021-01-11-delete js

--- src/assets/js/onepagewebheader.js
+++
@@ -1,8 +0,0 @@
-addEventListener("scroll",function(){
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky",window.scrollY > 0);
-})
-function toggle(){
-    var header = document.querySelector("header");
-    header.classList.toggle("active");
-}
