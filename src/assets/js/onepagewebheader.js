2957c43e73f5914fc962e1b8588062ae63500bc3
Nains <jason20226@gmail.com>
2021-01-11 10:31:54 +0800

    2021-01-11

---
+++ src/assets/js/onepagewebheader.js
@@ -0,0 +1,8 @@
+addEventListener("scroll",function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 0);
+})
+function toggle(){
+    var header = document.querySelector("header");
+    header.classList.toggle("active");
+}
